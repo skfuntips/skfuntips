501bddc2b92fa625ee32d7b568984c344cf3c9e2
unknown <unknown@unknown.com>
2013-01-15 07:11:16 +0800

    add ImageListParam

---
+++ src/param/ImageListParam.h
@@ -0,0 +1,16 @@
+#ifndef IMAGE_LIST_PARAM
+
+#include "IParam.h"
+
+class ImageListParam :public IParam{
+    Q_OBJECT
+
+    public:
+
+    ImageListParam(QObject * suoYouZhe=0);
+
+    private:
+
+
+};
+#endif//IMAGE_LIST_PARAM
