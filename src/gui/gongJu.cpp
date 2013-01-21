bc9d709ca95f9e65ac5b0aca733c79d6ebcbf9f2
unknown <unknown@unknown.com>
2013-01-21 14:23:52 +0800

    add gif picture load function

--- src/gui/gongJu.cpp
+++ src/gui/gongJu.cpp
@@ -2,6 +2,8 @@
 
 #include <QFileDialog>
 #include <QApplication>
+#include <QDebug>
+#include <QMovie>
 #include <QClipboard>
 #include <QFile>
 #include <QTextStream>
@@ -50,14 +52,45 @@ QString jianQieBanWenBen(QWidget* parent){
     return zhanTie;
 }
 
+static QList<QImage> loadGif(const QString& fileName){
+
+    QList<QImage> ret;
+
+    QMovie gif(fileName);
+    if(!gif.isValid()){
+        return ret;
+    }
+
+    gif.start();
+    gif.setPaused(true);
+
+    for(int i=0,size=gif.frameCount();i<size;++i){
+
+        QImage frame=gif.currentImage();
+
+        Q_ASSERT(!frame.isNull());
+
+        ret.append(frame);
+        gif.jumpToNextFrame();
+
+    }
+
+    return ret;
+
+}
 
 QList<QImage> loadImages(QWidget* parent){
     QStringList fileNames=QFileDialog::getOpenFileNames
-        (parent,QObject::tr("open file"),QString(),"*.bmp *.jpg *.png");
+        (parent,QObject::tr("open file"),QString(),"*.bmp *.jpg *.png *.gif");
 
     QList<QImage> ret;
 
     foreach(const QString& fileName,fileNames){
+
+        if(fileName.endsWith("gif")){
+            ret.append(loadGif(fileName));
+            continue;
+        }
 
         QImage image(fileName);
         if(!image.isNull()){
